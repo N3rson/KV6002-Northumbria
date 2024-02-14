1e4e27592e389e04238595a24e9b97afc599792c
Pawel Lasota <pawellasota2002@gmail.com>
2024-02-14 01:47:44 +0000

    Added a hamburger menu

---
+++ team-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
+import Menu from './components/Menu'
+
+function App() {
+
+
+  return (
+    <div>
+        <Menu />
+        <Routes>
+          <Route path = "/" element={<Home />} />
+        </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ team-app/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <h1>Home</h1>
+      </div>
+    )
+  }
+  
+  export default Home
