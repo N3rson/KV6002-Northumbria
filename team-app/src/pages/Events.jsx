ee19d8514107ae31f2ecee5ce9becd330b702b37
Pawel Lasota <pawellasota2002@gmail.com>
2024-02-27 18:28:11 +0000

    Removed Button from Events Page

--- team-app/src/pages/Events.jsx
+++ team-app/src/pages/Events.jsx
@@ -79,9 +79,6 @@ function Events() {
                     ))}
                 </div>
             </div>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
-              Book
-            </button>
         </div>
     )
 }
