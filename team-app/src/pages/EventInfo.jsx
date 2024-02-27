fa1106f88ad0656e1e93823934531cff098745e9
Pawel Lasota <pawellasota2002@gmail.com>
2024-02-27 17:22:26 +0000

    Return Button - Event Info

--- team-app/src/pages/EventInfo.jsx
+++ team-app/src/pages/EventInfo.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { collection, doc, getDoc } from 'firebase/firestore';
 import { firestore } from '../firebaseConfig';
+import backBtn from '../assets/back_button.png';
 
 function EventInfo() {
 
@@ -32,14 +33,11 @@ function EventInfo() {
 
     return (
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-        <h1>Event Information</h1>
-
-        <div key={event.id} className='border-b-2 border-black m-10'>
-            <h2 className='flex justify-center font-bold'>{event.EventName}</h2>
-            <p className='flex justify-center'>{event.EventCategory}</p>
-            <p className='flex justify-center'>{event.EventDate}</p>
-            <p className='flex justify-center'>{event.EventAttendance} / {event.EventLimit}</p>
-            <p className='flex justify-center'>{event.EventLocation}</p>
+        <img src={backBtn} alt="Back" className="h-6 w-6" />
+        <div key={event.id} className='border-b-2 border-black m-10 '>
+            <h2 className='flex justify-center font-bold text-xl border-b-2 border-b-black'>{event.EventName}</h2>
+            <p className='flex justify-center'>{event.EventDate} at {event.EventTime}</p>
+            <p className='flex justify-center mt-10'>{event.EventDescription}</p>
         </div>
 
       </div>
