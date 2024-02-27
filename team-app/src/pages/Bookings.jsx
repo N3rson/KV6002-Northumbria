0b91548bfec942537da542b124b49f792b9085c7
N3rson <124893280+N3rson@users.noreply.github.com>
2024-02-27 18:24:05 +0000

    fetching bookings

--- team-app/src/pages/Bookings.jsx
+++ team-app/src/pages/Bookings.jsx
@@ -1,7 +1,38 @@
+import { firestore } from '../firebaseConfig'
+import React, { useState, useEffect } from 'react'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+
 function Bookings() {
+
+  const [bookings, setBookings] = useState([])
+
+  useEffect(() => {
+    const fetchBookings = async () => {
+        try {
+            const bookingsCollection = collection(firestore, 'Bookings')
+            const bookingsSnapshot = await getDocs(bookingsCollection)
+            const bookingsData = bookingsSnapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }))
+            setBookings(bookingsData)
+        } catch (error) {
+            console.error('Error fetching bookings: ', error)
+        }
+    }
+    fetchBookings()
+  }, [])
+
     return (
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
         <h1>Bookings</h1>
+        {bookings.map(booking => (
+          <div key={booking.id} className='bg-white rounded-xl border-black m-10'>
+           <h2 className='flex justify-center font-bold'>{booking.BookingDate}</h2>
+           <h2 className='flex justify-center font-bold'>{booking.BookingTime}</h2>
+           <h2 className='flex justify-center font-bold'>event name</h2>
+           </div>
+          ))}
       </div>
     )
   }
