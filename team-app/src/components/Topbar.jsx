d1c520059666202fb63d1c7e8922d8eb37ca1a7e
sayedo2121 <sayedo2121@gmail.com>
2024-03-01 23:46:23 +0000

    fetched notifications from database

--- team-app/src/components/Topbar.jsx
+++ team-app/src/components/Topbar.jsx
@@ -1,14 +1,21 @@
 import { Link, useLocation  } from 'react-router-dom'
+import icon_notifications from '../assets/icon_notifications.png'
+import icon_notifications_full from '../assets/icon_notifications_filled.png'
 
 function Topbar(props){
     return (
         <>
-            <div className="Topbar flex flex-row items-center">
+            <div className="Topbar flex flex-row items-center bg-white shadow-middle mb-5 p-4">
                 <div>
-                    <h1 className='text-bold'>{props.pageName}</h1>
+                    <h1 className='font-bold'>{props.pageName}</h1>
                 </div>
                 <div className='ml-auto'>
-                    <Link to="/notifications"><span className="">Notifications</span></Link>
+
+                    <Link to="/notifications">
+                        { props.pageName != 'Notifications' && <img src={icon_notifications} alt="notifications" className='h-6 w-6'/>}
+                        { props.pageName == 'Notifications' && <img src={icon_notifications_full} alt="notifications" className='h-6 w-6'/>}
+                    </Link>
+
                 </div>
                 
             </div>
