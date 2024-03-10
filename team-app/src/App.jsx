0827397b11919b6bce7978d7b0758b15fb857589
Lucie <99190663+Lucie-22@users.noreply.github.com>
2024-03-10 22:41:30 +0000

    Background Circle Changes

--- team-app/src/App.jsx
+++ team-app/src/App.jsx
@@ -56,14 +56,17 @@ function App() {
   return (
     //<div className='bg-gradient-to-r from-primary to-secondary text-black text-lg min-h-screen'>
     <div className="overflow-x-hidden relative text-black text-lg min-h-screen"> 
-      <div className="">
-        <div className="absolute -top-40 -left-40 -z-10 mix-blend-multiply w-96 h-96 blur-2xl bg-primary rounded-full"></div>
-        <div className="absolute top-10 left-80 -z-10 mix-blend-multiply w-80 h-80 blur-2xl bg-secondary rounded-full"></div>
-        <div className="absolute -top-10 left-40 -z-10 mix-blend-multiply w-48 h-48 blur-2xl bg-secondary rounded-full"></div>
+      <div>
+        <div className="absolute -top-20 -left-40 -z-10 mix-blend-multiply w-96 h-96 blur-2xl bg-primary rounded-full"></div>
+        <div className="absolute -top-20 left-40 -z-10 mix-blend-multiply w-80 h-80 blur-2xl bg-secondary rounded-full"></div>
+        <div className="absolute top-40 -right-20 -z-10 mix-blend-multiply w-48 h-64 blur-2xl bg-secondary rounded-full"></div>
+        <div className="absolute top-20 left-20 -z-10 mix-blend-multiply w-48 h-64 blur-2xl bg-primary rounded-full"></div>
 
-        <div className="absolute -bottom-10 -right-60 -z-10 mix-blend-multiply w-96 h-96 blur-2xl bg-primary rounded-full"></div>
-        <div className="absolute bottom-0 -left-40 -z-10 mix-blend-multiply w-96 h-96 blur-2xl bg-secondary rounded-full"></div>
-        <div className="absolute bottom-40 left-20 -z-10 mix-blend-multiply w-64 h-64 blur-2xl bg-primary rounded-full"></div>
+        <div className="absolute top-40 -left-40 -z-10 mix-blend-multiply w-64 h-96 blur-2xl bg-secondary rounded-full"></div>
+        <div className="absolute bottom-40 -right-20 -z-10 mix-blend-multiply w-64 h-64 blur-2xl bg-secondary rounded-full"></div>
+
+        <div className="absolute -bottom-20 -right-20 -z-10 mix-blend-multiply w-96 h-96 blur-2xl bg-primary rounded-full"></div>
+        <div className="absolute -bottom-40 -left-40 -z-10 mix-blend-multiply w-96 h-96 blur-2xl bg-primary rounded-full"></div>
       </div>
       <Topbar pageName={thisPageName} />
       <Navbar />
