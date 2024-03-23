c4a56b02707aa38090cb263150e89802cf22a108
sayedo2121 <sayedo2121@gmail.com>
2024-03-23 22:43:55 +0000

    notifications final

--- team-app/src/NotificationsUtil.jsx
+++ team-app/src/NotificationsUtil.jsx
@@ -1,32 +1,77 @@
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs, addDoc, query, where } from "firebase/firestore";
 import { firestore } from "./firebaseConfig";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { auth } from "./firebaseConfig";
+import { Link } from "react-router-dom";
 
+/**
+ * Notifications Utility
+ *
+ * This component is responsible for checking for new notifications and populating the notification table
+ *
+ * @category Page
+ * @author Sayed Husain Kadhem
+ */
 function NotificationsUtil(props) {
+  const currentUser = auth.currentUser;
+  const [fetchedBookings, setFetchedBookings] = useState(false);
+  const [userAccepted, setUserAccepted] = useState(Notification.permission === "granted"? true:false);
+
+  const askPermission = async () => {
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      return true;
+    } else if (Notification.permission !== "denied") {
+      let permission = await Notification.requestPermission();
+      if (permission === "granted") {
+        setUserAccepted(true);
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
+
+  askPermission();
+
   useEffect(() => {
-    fetchEvents().then((newEvents) => {
-      if (!props.eventsFetched) {
-        props.setEvents(newEvents);
-        props.setEventsFetched(true);
-        newEvents.forEach((val) => {
-          checkTicketLimit(val);
+    if (userAccepted) {
+      fetchEvents().then((newEvents) => {
+        if (!props.eventsFetched) {
+          props.setEvents(newEvents);
+          props.setEventsFetched(true);
+          newEvents.forEach((val) => {
+            checkTicketLimit(val);
+          });
+        }
+      });
+
+      if (!fetchedBookings) {
+        fetchBookings().then((newBookings) => {
+          checkBookings(newBookings);
+          setFetchedBookings(true);
         });
       }
-    });
 
-    const interval = setInterval(() => {
-      NotificationInterval();
-    }, 36000000); //10 hours
+      const interval = setInterval(() => {
+        NotificationInterval();
+        console.log("notification interval");
+      }, 36000000); //10 hours
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, [props.events]);
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [props.events, userAccepted]);
 
   const NotificationInterval = async () => {
     const newEvents = await fetchEvents();
+    const newBookings = await fetchBookings();
 
-    checkEvents(newEvents);
+    if (newEvents) checkEvents(newEvents);
+
+    //if (newBookings) checkBookings(newBookings);
   };
 
   const fetchEvents = async () => {
@@ -44,35 +89,74 @@ function NotificationsUtil(props) {
     }
   };
 
+  const fetchBookings = async () => {
+    try {
+      const bookingsCollection = collection(firestore, "Bookings");
+      const userBookingsQuery = query(
+        bookingsCollection,
+        where("userId", "==", currentUser.uid)
+      );
+      const bookingsSnapshot = await getDocs(userBookingsQuery);
+      const bookingsData = bookingsSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      return bookingsData;
+    } catch (error) {
+      console.error("Error fetching bookings: ", error);
+    }
+  };
+
   const checkEvents = (newEvents) => {
     const eventIds = props.events.map((e) => e.id);
     newEvents.forEach((val, index) => {
       if (!eventIds.includes(val.id)) {
-        showNotification(val, "A new event has been added!");
+        showNotification(val, "A new event has been added!", "events");
         checkTicketLimit(val);
         props.setEvents(newEvents);
       }
     });
   };
 
-  const showNotification = (val, text) => {
+  const checkBookings = (newBookings) => {
+    let today = new Date();
+    newBookings.forEach((val, index) => {
+      if (val.EventDate) {
+        let eventDate = new Date(val.EventDate);
+        let Difference_In_Time = eventDate.getTime() - today.getTime();
+        let Difference_In_Days =
+          Math.round(Difference_In_Time / (1000 * 3600 * 24)) + 1;
+
+        if (Difference_In_Days <= 5 && Difference_In_Days >= 0)
+          showNotification(
+            val,
+            "You have a booked event in " + Difference_In_Days + " days",
+            "bookings"
+          );
+      }
+    });
+  };
+
+  const showNotification = (val, text, type) => {
     props.toast.custom((t) => (
       <div
         className={`${
           t.visible ? "animate-enter" : "animate-leave"
         } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
       >
-        <div className="flex-1 w-0 p-4">
-          <div className="flex items-start">
-            <div className="ml-3 flex-1">
-              <p className="text-sm font-medium text-gray-900">{text}</p>
-              <p className="mt-1 text-sm text-gray-500">{val.EventName}</p>
+        <Link className="flex-1 w-0 " to="/notifications">
+          <div className="p-4">
+            <div className="flex items-start">
+              <div className="ml-3 flex-1">
+                <p className="text-sm font-medium text-gray-900">{text}</p>
+                <p className="mt-1 text-sm text-gray-500">{val.EventName}</p>
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
         <div className="flex border-l border-gray-200">
           <button
-            onClick={() => toast.dismiss(t.id)}
+            onClick={() => props.toast.dismiss(t.id)}
             className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
           >
             Close
@@ -80,6 +164,10 @@ function NotificationsUtil(props) {
         </div>
       </div>
     ));
+    
+    
+    addToHistory(val, text, type);
+    sendDesktopNotification(val, text);
   };
 
   const checkTicketLimit = (val) => {
@@ -88,9 +176,46 @@ function NotificationsUtil(props) {
       ticketsLeft = parseInt(val.EventLimit) - parseInt(val.EventAttendance);
 
     if (ticketsLeft != undefined && ticketsLeft <= 5 && ticketsLeft > 0) {
-      showNotification(val, "This event has less that 5 tickets left");
+      showNotification(
+        val,
+        "This event has less that 5 tickets left",
+        "events"
+      );
     }
   };
+
+  const addToHistory = async (val, text, type) => {
+    //Responsible for adding the notification to the database
+    const notificationRef = collection(firestore, "Notifications");
+    let notificationDocRef = "";
+    let today = new Date();
+    if (type == "events") {
+      notificationDocRef = await addDoc(notificationRef, {
+        notificationName: val.EventName,
+        notificationEventRef: val.id,
+        notificationDesc: text,
+        notificationType: type,
+        notificationDate: today.toISOString(),
+        userId: currentUser.uid,
+      });
+    } else if (type == "bookings") {
+      notificationDocRef = await addDoc(notificationRef, {
+        notificationName: val.EventName,
+        notificationBookingRef: val.id,
+        notificationDesc: text,
+        notificationType: type,
+        notificationDate: today.toISOString(),
+        userId: currentUser.uid,
+      });
+      console.log(notificationDocRef);
+    }
+  };
+
+  const sendDesktopNotification = (val, text)=>{
+    let notification = new Notification(val.EventName, {
+      body: text
+    })
+  }
 }
 
 export default NotificationsUtil;
--- team-app/src/pages/Notifications.jsx
+++ team-app/src/pages/Notifications.jsx
@@ -1,40 +1,97 @@
-import React, { useState, useEffect } from 'react'
-import { collection, getDocs, query, where } from 'firebase/firestore'
-import { firestore } from '../firebaseConfig'
+import React, { useState, useEffect } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { firestore } from "../firebaseConfig";
+import { auth } from "../firebaseConfig";
+import { Link } from "react-router-dom";
 
-function Notifications(){
-    const [notifications, setnotifications] = useState([])
+/**
+ * Notifications Page
+ *
+ * This page shows a history of notifications
+ *
+ * @category Page
+ * @author Sayed Husain Kadhem
+ */
+function Notifications() {
+  const [notifications, setnotifications] = useState([]);
+  const currentUser = auth.currentUser;
 
-    useEffect(() => {
-        const fetchnotifications = async () => {
-            try {
-                const notificationsCollection = collection(firestore, 'Notifications')
-                const notificationsSnapshot = await getDocs(notificationsCollection)
-                const notificationsData = notificationsSnapshot.docs.map(doc => ({
-                    id: doc.id,
-                    ...doc.data()
-                }))
-                setnotifications(notificationsData)
-            } catch (error) {
-                console.error('Error fetching notifications: ', error)
-            }
-        }
+  function minutesAgo(dateString) {
+    const pastDate = new Date(dateString);
+    const currentDate = new Date();
+    const differenceInMilliseconds = currentDate - pastDate;
+    const differenceInMinutes = Math.floor(differenceInMilliseconds / 60000);
+    return differenceInMinutes;
+  }
 
-        fetchnotifications()
-    }, [])
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      try {
+        const notificationsCollection = collection(firestore, "Notifications");
+        const userNotificationsQuery = query(
+          notificationsCollection,
+          where("userId", "==", currentUser.uid)
+        );
+        const notificationsSnapshot = await getDocs(userNotificationsQuery);
+        const notificationsData = notificationsSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setnotifications(notificationsData);
+      } catch (error) {
+        console.error("Error fetching notifications: ", error);
+      }
+    };
 
-    return (
-        <div>
-            <div className="flex flex-col items-center gap-6 mx-6">
-                    {notifications.map(notification => (
-                        <div key={notification.id} className='backdrop-blur-sm w-full p-3 shadow-middle bg-white/30 rounded-lg'>
-                            <h2 className='pb-1 font-semibold text-colour1'>{notification.title}</h2>
-                            <p className='flex text-s'>{notification.message}</p>
-                        </div>
-                    ))}
-            </div>
+    fetchNotifications();
+  }, []);
+
+  return (
+    <div>
+      <div className="flex flex-col items-center gap-6 mx-6">
+        {notifications.map((notification) => {
+          let link = "";
+
+          let minutes = minutesAgo(notification.notificationDate);
+
+          if (minutes == 0) {
+            minutes = "just now";
+          } else if (minutes >= 60) {
+            minutes = Math.round(minutes / 60.0).toString() + ' hours ago';
+          } else {
+            minutes = minutes + " minutes ago";
+          }
+
+          if (notification.notificationType == "events")
+            link = "/event/" + notification.notificationEventRef;
+          else if (notification.notificationType == "bookings")
+            link = "/booking/" + notification.notificationBookingRef;
+          return (
+            <Link className="w-full" key={notification.id} to={link}>
+              <div className="w-full backdrop-blur-sm  p-3 shadow-middle bg-white/30 rounded-lg">
+                <div className="flex flex-row items-center">
+                  <h2 className="pb-1 font-semibold text-colour1">
+                    {notification.notificationName}
+                  </h2>
+                  <div className="ml-auto">
+                    <p className="text-xs">{minutes}</p>
+                  </div>
+                </div>
+
+                <p className="flex text-s">{notification.notificationDesc}</p>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+
+      {notifications.length == 0 && (
+        <div className="text-center">
+          <p>You have no recent notifications</p>
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default Notifications
+export default Notifications;
